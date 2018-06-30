b685dd28cf3c81b31c5a6f507c12f9d65cf231f7
Prakash Saini <prakashw3expert@gmail.com>
2018-06-30 16:58:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ class SettingsList extends React.Component {
         {this._getGroups().map((group, index) => {
           return this._groupView(group, index);
         })}
+        {this.props.renderFooter()}
       </ScrollView>
     )
   }
